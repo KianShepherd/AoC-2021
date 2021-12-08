0c12107ef6071a4b8e82f84f5cc6eb8267837a9b
Kian Shepherd <kianshepherd73@gmail.com>
2021-12-07 23:09:35 -0500

    created filtering function

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,6 +6,8 @@ struct Counter {
     zero_count: i32,
 }
 
+fn filter_arr(nums: Vec<Vec<i32>>, index: usize, looking_for: i32) {}
+
 fn main() {
     let contents = fs::read_to_string("input").expect("Something went wrong reading the file");
     let v: Vec<&str> = contents.split('\n').collect();
