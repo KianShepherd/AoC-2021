4db97cfd70a5b5a04c2fe3106004552db1ea922c
Kian Shepherd <kianshepherd73@gmail.com>
2021-12-13 08:37:16 -0500

    Init next few days

---
+++ day12/src/main.rs
@@ -0,0 +1,8 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("input").expect("Something went wrong reading the file");
+    let v: Vec<&str> = contents.trim().split('\n').collect::<Vec<&str>>();
+
+    println!("{:?}", v);
+}
